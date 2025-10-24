b05507af7d08a4282a2a63bc2b0988bee2a52725
Andrew Sou <aksou@uci.edu>
2025-10-24 00:09:48 -0700

    Added earliest and latest tweet times displaying in DOM

--- js/about.js
+++ js/about.js
@@ -8,6 +8,15 @@ function parseTweets(runkeeper_tweets) {
 	tweet_array = runkeeper_tweets.map(function(tweet) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
+
+	const times = tweet_array.map( tweet => tweet.time.getTime());
+	const earliestTweet = new Date(Math.min(...times)).toLocaleString('en-US', {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'});
+	const latestTweet = new Date(Math.max(...times)).toLocaleString('en-US', {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'});
+
+	console.log(earliestTweet)
+	console.log(latestTweet)
+	document.getElementById('firstDate').innerText = earliestTweet;
+	document.getElementById('lastDate').innerText = latestTweet;
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
